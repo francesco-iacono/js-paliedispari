26e211c66f24064e042a5059d92ef88635ce55dc
francesco <f.iacono89@gmail.com>
2020-11-18 20:54:02 +0100

    finito

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -10,16 +10,21 @@ console.log(confronto);
 // Restituisce true se palindroma, false altrimenti
 function checkPalindromo(parola) {
   var contrario = "";
+  // Cambia la stringa in minuscolo
+  var parolaMinuscola = parola.toLowerCase();
 
   // Devo creare parola al contrario
   for (var i = parola.length - 1; i >= 0; i--) {
     contrario += parola[i];
   }
   console.log("Parola al contrario", contrario);
+  var messaggio = "";
   // Confronto parola e contrario per vedere se sono uguali
-  if (contrario.toLowerCase() == parolaMinuscola.toLowerCase()) {
-    return true;
+  if (contrario.toLowerCase() == parolaMinuscola) {
+    messaggio = "La tua parola è un palindromo";
+    return messaggio;
   } else {
-    return false;
+    messaggio = "La tua parola non è un palindromo";
+    return messaggio;
   }
 }
